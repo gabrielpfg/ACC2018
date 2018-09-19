927cbfd5419a8dab473b8c4afdbefc13a3ca17df
gabrielpfg <43424334+gabrielpfg@users.noreply.github.com>
2018-09-19 19:39:17 -0300

    Add files via upload

---
+++ gains_acc2019.m
@@ -0,0 +1,35 @@
+load('gains_acc2019')
+
+% Non-scheduling solution for the NRC
+single_F;
+single_Ec;
+
+% Scheduling solution for the NRC with AW
+F1_aw;
+F2_aw;
+F3_aw;
+F4_aw;
+Ec1_aw;
+Ec2_aw;
+Ec3_aw;
+Ec4_aw;
+
+% Scheduling solution for the NRC without AW
+F1_aw0;
+F2_aw0;
+F3_aw0;
+F4_aw0;
+Ec1_aw0;
+Ec2_aw0;
+Ec3_aw0;
+Ec4_aw0;
+
+% Scheduling solution for the traditional RC with AW
+F1_res;
+F2_res;
+F3_res;
+F4_res;
+Ec1_res;
+Ec2_res;
+Ec3_res;
+Ec4_res;
